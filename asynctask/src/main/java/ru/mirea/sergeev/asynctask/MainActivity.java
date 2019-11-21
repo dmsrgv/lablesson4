93919edb823b6870a9e466bc116b2ee3c78bbb11
dmsrgv <ucozar@gmail.com>
2019-11-21 16:55:58 +0300

    Done

---
+++ asynctask/src/main/java/ru/mirea/sergeev/asynctask/MainActivity.java
@@ -0,0 +1,22 @@
+package ru.mirea.sergeev.asynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void getResult(View view) {
+        MyAsyncTask myAsyncTask = new MyAsyncTask(textView);
+        myAsyncTask.execute();
+    }
+}
